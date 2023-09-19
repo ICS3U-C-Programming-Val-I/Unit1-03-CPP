5cf5e9125eb1c133fac7e80174094edd6edce0f5
Val Ijaola <valian.ijaola@stu.ocsb.ca>
2023-09-19 00:05:07 +0000

    Added helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2023 Val I All rights reserved
+// .
+// Created By: Val Ijaola
+// Date: September 18, 2023
+// This program says hello world
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+}
